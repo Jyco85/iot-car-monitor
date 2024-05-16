65ab066f9268372ab54b66020904ad63a7bc1d29
Jyco85 <cruzolivarezjesusyahir@gmail.com>
2024-05-16 11:49:44 -0600

    Add files via upload

---
+++ monitor/js/main.js
@@ -0,0 +1,43 @@
+alert("Hola mundo");
+
+
+
+
+let mensaje=document.getElementById('respuesta');
+
+function callApiRequest() {
+  // Hacer una petición para un usuario con ID especifico
+  axios
+    .get(
+      "http://localhost/iot-car-control/back-end/apis/getRegistro.php" )
+    .then(function (response) {
+      // manejar respuesta exitosa
+      console.log(response);
+
+      let respuestaServidor = 
+      response.data == "F"
+      ? "ADELANTE"
+
+    :response.data == "B"
+      ? "ATRAS"
+
+     : response.data == "R"
+      ? "IZQUIERDA"
+
+      :response.data == "L"
+      ? "DERECHA": "DETENER";
+
+
+      respuesta.innerHTML="Respuesta:" + respuestaServidor ;
+    })
+    .catch(function (error) {
+      // manejar error
+      console.log(error);
+    })
+    .finally(function () {
+      // siempre sera executado
+    });
+  }
+
+  setInterval(callApiRequest, 2000);
+    
