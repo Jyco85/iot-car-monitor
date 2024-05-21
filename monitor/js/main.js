0cc6d36c6a9f47567311fb070ef8a63501bb746d
Jyco85 <cruzolivarezjesusyahir@gmail.com>
2024-05-21 11:11:11 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -9,7 +9,7 @@ function callApiRequest(estatus) {
   // Hacer una petición para un usuario con ID especifico
   axios
     .get(
-      "https://18.116.14.205/iot-car-control/back-end/apis/getRegistro.php" )
+      "https://3.23.103.41/iot-car-control/back-end/apis/getRegistro.php" )
     .then(function (response) {
       // manejar respuesta exitosa
       console.log(response);
