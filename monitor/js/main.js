c9fac7cdd41e728818f97f70e494686cd6191865
Jyco85 <cruzolivarezjesusyahir@gmail.com>
2024-05-24 09:58:18 -0600

    Update main.js

--- monitor/js/main.js
+++ monitor/js/main.js
@@ -1,31 +1,26 @@
-alert("Hola mundo");
+alert("Hola Mundo");
 
-let mensaje=document.getElementById('respuesta');
+let Respuesta = document.getElementById("Respuesta");
 
-function CallApiRequest(estatus) {
+function CallApiRequest() {
   // Hacer una petición para un usuario con ID especifico
   axios
-    .get(
-      "http://3.23.103.41/iot-car-control/back-end/apis/getRegistro.php" )
+    .get("http://18.217.27.112/iot-car-control/back-end/apis/getRegistro.php")
     .then(function (response) {
       // manejar respuesta exitosa
       console.log(response);
 
-      let respuestaServidor = 
-      response.data == "F"
-      ? "ADELANTE"
-
-    :response.data == "B"
-      ? "ATRAS"
-
-     : response.data == "R"
-      ? "IZQUIERDA"
-
-      :response.data == "L"
-      ? "DERECHA": "DETENER";
-
-
-      respuesta.innerHTML="Respuesta: <strong>" + respuestaServidor + "</strong>" ;
+      let respuestaServidor =
+        response.data == "F"
+          ? "Adelante"
+          : response.data == "B"
+          ? "Atras"
+          : response.data == "L"
+          ? "Izquierda"
+          : response.data == "R"
+          ? "Derecha"
+          : "Detener";
+      Respuesta.innerHTML = "Respuesta: <strong>" + respuestaServidor + "</strong>";
     })
     .catch(function (error) {
       // manejar error
@@ -34,9 +29,9 @@ function CallApiRequest(estatus) {
     .finally(function () {
       // siempre sera executado
     });
-  }
+}
 
-  setInterval(CallApiRequest, 2000);
+setInterval(CallApiRequest, 2000);
     
 
     
